3b9dab0289c73a7b2b69b810368e919d012711a8
Alwin Brauns <63373755+AlwinBrauns@users.noreply.github.com>
2022-11-16 19:27:39 +0100

    text

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const path = require('path')
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log(`Example app listening on port ${process.env.PORT || 3333}`)
+  console.log(`listening on port ${process.env.PORT || 3333}`)
 })
