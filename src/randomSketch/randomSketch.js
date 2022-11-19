e49596b0ac29aa555f9d64ac106257fa372b5dca
Alwin Brauns <63373755+AlwinBrauns@users.noreply.github.com>
2022-11-19 10:18:19 +0100

    walker

--- src/randomSketch/randomSketch.js
+++ src/randomSketch/randomSketch.js
@@ -8,12 +8,30 @@ export default function randomSketch(s) {
             this.y = canvasHeight / 2;
         }
         display() {
-            s.stroke(3);
-            s.point(this.x,this.y);
+            s.stroke(255)
+            s.point(this.x, this.y)
+        }
+        step() {
+            const possibleChoices = 4;
+            const choice = Math.floor(Math.random()*(possibleChoices+1))
+            switch (choice) {
+                case 0:
+                    this.x++;
+                break;
+                case 1:
+                    this.x--;
+                break;
+                case 2:
+                    this.y++;
+                break;
+                case 3:
+                    this.y--;
+                break;
+            }
         }
     }
 
-    const walker = new Walker(s, canvasWidth, canvasHeight);
+    const walker = new Walker(canvasWidth, canvasHeight);
 
     s.setup = () => {
         s.createCanvas(canvasWidth, canvasHeight).parent("p5");
@@ -23,5 +41,6 @@ export default function randomSketch(s) {
     s.draw = () => {
         s.background(10);
         walker.display();
+        walker.step();
     }
 }
