ed4ec7311e66aadb1aea48cf9729f9aa19a44497
Alwin Brauns <63373755+AlwinBrauns@users.noreply.github.com>
2022-11-19 12:48:37 +0100

    random to mouse

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const fps = 60;
-export const canvasHeight = window.innerHeight;
+export const canvasHeight = window.innerHeight - 20;
 export const canvasWidth = window.innerWidth;
--- src/randomSketch/randomSketch.js
+++ src/randomSketch/randomSketch.js
@@ -23,16 +23,40 @@ export default function randomSketch(s) {
                 graphic.point(p.x, p.y)
             }
         }
-        make(size) {
+        make(size, stepFunction) {
             this.points = []
             for(let i = 0; i<size;i++) {
                 this.points.push(graphic.createVector(this.x,this.y))
-                this.step()
+                stepFunction()
             }
         }
-        step() {
+        step(gravitateX = 0, gravitateY = 0) {
+            this.x += s.random(-1,1) + gravitateX
+            this.y += s.random(-1,1) + gravitateY
+        }
+        stepPercentage(propR = 0.5, propU = 0.5) {
+            //bedingung => 50% = (propR + probL) = LR
+            //bedingung => 50% = (propU + propD) = UD
+            //bedingung => 100% = LR + UD
+            propR = propR/2
+            propU = propU/2
+            const propL = 0.5-propR
+            const propD = 0.5-propU
+
+            const random = s.random(0,1);
+            if(random<(0+propR)) {
+                this.x++;
+            }else if (random<0+propR+propL) {
+                this.x--;
+            }else if (random<0+propR+propL+propD) {
+                this.y++;
+            }else {
+                this.y--;
+            }
+        }
+        simpleStep() {
             const possibleChoices = 4;
-            const choice = Math.floor(Math.random()*(possibleChoices+1))
+            const choice = Math.floor(s.random(0, possibleChoices))
             switch (choice) {
                 case 0:
                     this.x++;
@@ -51,23 +75,24 @@ export default function randomSketch(s) {
     }
 
     const walker = new Walker(canvasWidth, canvasHeight);
-
+    const step = () => {
+        walker.stepPercentage((s.mouseX/canvasWidth), 1-(s.mouseY/canvasHeight))
+    }
     s.setup = () => {
         s.createCanvas(canvasWidth, canvasHeight).parent("p5");
         graphic = s.createGraphics(canvasWidth, canvasHeight)
         s.frameRate(fps);
         s.background(10);
-        walker.make(walkSize);
+        walker.make(walkSize, step);
         walker.display();
     }
 
     s.draw = () => {
         if((s.frameCount % (fps*changeOnSecond)) === 0){
-            s.clear();
             graphic.clear();
             s.background(10);
             walker.initial();
-            walker.make(walkSize);
+            walker.make(walkSize, step);
             walker.display();
         }
         s.image(graphic, 0, 0)
